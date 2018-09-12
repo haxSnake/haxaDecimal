36c9dada46bab68e6584fe3829def4ac6fd7f709
Kieran813 <43210075+Kieran813@users.noreply.github.com>
2018-09-12 17:18:12 +0100

    12/09/18 - Article 2
    SQL connection finished, buttons WIP, no article text

--- hexaDECIMAL/hexaDECIMAL/UcArticle.Designer.cs
+++ hexaDECIMAL/hexaDECIMAL/UcArticle.Designer.cs
@@ -28,49 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1468, 200);
-            this.panel1.TabIndex = 4;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::hexaDECIMAL.Properties.Resources.exitX;
-            this.pictureBox1.Location = new System.Drawing.Point(1430, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // UcArticle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(154)))), ((int)(((byte)(70)))));
-            this.Controls.Add(this.panel1);
             this.Name = "UcArticle";
             this.Size = new System.Drawing.Size(1468, 837);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- hexaDECIMAL/hexaDECIMAL/UcArticle.cs
+++ hexaDECIMAL/hexaDECIMAL/UcArticle.cs
@@ -7,14 +7,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Net.Sockets;
 
 namespace hexaDECIMAL
 {
     public partial class UcArticle : UserControl
     {
+
+        Button[] btnButtons = new Button[25];
+        List<articleClass> articleList = new List<articleClass>();
+        articleClass[] articleArray;
+        String querySql = "";
+
+
         public UcArticle()
         {
             InitializeComponent();
+        }
+
+        private void CurrentSquadUC_Load(object sender, EventArgs e)
+        {
+            //ONLINE MODE
+
+            try
+            {
+                //OPEN MYSQL CONNECTION
+                using (MySqlConnection con = new MySqlConnection(databaseOperations.mysqlDatasource))
+                {
+                    con.Open();
+                    //PULL DATA FROM MYSQL DB
+                    MySqlCommand cmd = con.CreateCommand();
+                    cmd.CommandText = "select * from article order by artId asc";
+
+                    //CREATE ADAPTER AND TABLE TO PROCESS DATA
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    //PREPARE BUTTON DIMENSIONS
+                    int btnWidth = 180;
+                    int btnHeight = ClientSize.Height / 25;
+                    int btnLeft = 0;
+                    int btnTop = ClientSize.Height - (25 * btnHeight);
+
+                    //GENERATE BUTTONS AND MANAGE PLAYER DATA
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        //TAKE PLAYER INFO
+                        articleClass article = new articleClass("", "", "","");                        
+                        article.ArtTitle = dt.Rows[i]["artTitle"].ToString();
+                        article.ArtText = dt.Rows[i]["artText"].ToString();
+                        article.ArtAuthor = dt.Rows[i]["artAuthor"].ToString();
+                        article.ArtDate = dt.Rows[i]["artDate"].ToString();
+
+                        //COPY PLAYER TO THE LIST
+                        articleList.Add(article);
+
+                        //CREATE BUTTON
+                        btnButtons[i] = new Button();
+                        btnButtons[i].Text = article.ArtTitle; //VISIBLE BUTTON TEXT
+                        btnButtons[i].Width = btnWidth;
+                        btnButtons[i].Height = btnHeight;
+                        btnButtons[i].Left = btnLeft;
+                        btnButtons[i].Top = btnTop;
+                        btnButtons[i].BringToFront();
+                        btnButtons[i].BackColor = Color.FromArgb(22, 67, 152);
+                        btnButtons[i].ForeColor = Color.White;
+                        btnButtons[i].TabStop = false;
+                        btnButtons[i].FlatStyle = FlatStyle.Flat;
+                        btnButtons[i].FlatAppearance.BorderSize = 0;
+                        btnButtons[i].FlatAppearance.BorderColor = Color.FromArgb(22, 67, 152);
+                        btnButtons[i].Font = new Font("Century Gothic", 12);
+                        Controls.Add(btnButtons[i]);
+                        btnButtons[i].Click += new EventHandler(btnMySQL_Click);
+                        btnTop += btnHeight;
+                    }
+                }
+                //MOVE PLAYERS TO AN ARRAY
+                articleArray = articleList.ToArray();
+
+                //PRESENT FIRST PLAYER ON INITIAL LOAD
+                articleArtId.Text = articleArray[0].ArtTitle;
+                playerPosition.Text = articleArray[0].ArtText;
+                playerDob.Text = articleArray[0].ArtAuthor;
+                playerPreviousClub.Text = articleArray[0].ArtDate; 
+
+            }
+            catch (SocketException ex)
+            {
+                if (databaseOperations.errorMsg == false)
+                {
+                    MessageBox.Show(ex.Message, "Connection error");
+                    databaseOperations.errorMsg = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (databaseOperations.errorMsg == false)
+                {
+                    MessageBox.Show(ex.Message, "ERROR!");
+                    databaseOperations.errorMsg = true;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
