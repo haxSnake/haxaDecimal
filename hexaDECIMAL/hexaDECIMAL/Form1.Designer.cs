8a278019b072135c16a38151fdae2c00abbb26c6
haxSnake <silentsnake1986@gmail.com>
2018-09-13 12:14:01 +0100

    login screen update

--- hexaDECIMAL/hexaDECIMAL/Form1.Designer.cs
+++ hexaDECIMAL/hexaDECIMAL/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.loginButton = new System.Windows.Forms.Button();
             this.loginBox = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.regButton = new System.Windows.Forms.Button();
             this.pswBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -41,39 +44,51 @@
             // loginButton
             // 
             this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(455, 535);
+            this.loginButton.Location = new System.Drawing.Point(341, 435);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(150, 35);
+            this.loginButton.Size = new System.Drawing.Size(112, 28);
             this.loginButton.TabIndex = 0;
-            this.loginButton.Text = "button1";
+            this.loginButton.Text = "Log in";
             this.loginButton.UseVisualStyleBackColor = true;
             this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // loginBox
             // 
             this.loginBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginBox.Location = new System.Drawing.Point(455, 378);
+            this.loginBox.Location = new System.Drawing.Point(341, 307);
+            this.loginBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.loginBox.Name = "loginBox";
-            this.loginBox.Size = new System.Drawing.Size(450, 34);
+            this.loginBox.Size = new System.Drawing.Size(338, 28);
             this.loginBox.TabIndex = 2;
+            this.loginBox.Text = "Email";
+            this.loginBox.Click += new System.EventHandler(this.loginBox_Click);
             this.loginBox.TextChanged += new System.EventHandler(this.loginBox_TextChanged);
+            this.loginBox.Leave += new System.EventHandler(this.loginBox_Leave);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1360, 200);
+            this.panel1.Size = new System.Drawing.Size(1020, 162);
             this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::hexaDECIMAL.Properties.Resources.exitX;
-            this.pictureBox1.Location = new System.Drawing.Point(1322, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(992, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.Size = new System.Drawing.Size(26, 28);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -82,37 +97,71 @@
             // regButton
             // 
             this.regButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.regButton.Location = new System.Drawing.Point(755, 535);
+            this.regButton.Location = new System.Drawing.Point(566, 435);
+            this.regButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.regButton.Name = "regButton";
-            this.regButton.Size = new System.Drawing.Size(150, 35);
+            this.regButton.Size = new System.Drawing.Size(112, 28);
             this.regButton.TabIndex = 5;
-            this.regButton.Text = "button2";
+            this.regButton.Text = "Register";
             this.regButton.UseVisualStyleBackColor = true;
             this.regButton.Click += new System.EventHandler(this.regButton_Click);
             // 
             // pswBox
             // 
             this.pswBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pswBox.Location = new System.Drawing.Point(455, 454);
+            this.pswBox.Location = new System.Drawing.Point(341, 369);
+            this.pswBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pswBox.Name = "pswBox";
-            this.pswBox.Size = new System.Drawing.Size(450, 34);
+            this.pswBox.Size = new System.Drawing.Size(338, 28);
             this.pswBox.TabIndex = 6;
+            this.pswBox.Text = "Password";
+            this.pswBox.Click += new System.EventHandler(this.pswBox_Click);
             this.pswBox.TextChanged += new System.EventHandler(this.pswBox_TextChanged);
+            this.pswBox.Enter += new System.EventHandler(this.pswBox_Enter);
+            this.pswBox.Leave += new System.EventHandler(this.pswBox_Leave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(341, 401);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(121, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Remember Me";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(490, 474);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(154)))), ((int)(((byte)(70)))));
-            this.ClientSize = new System.Drawing.Size(1360, 768);
+            this.ClientSize = new System.Drawing.Size(1020, 624);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.pswBox);
             this.Controls.Add(this.regButton);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.loginBox);
             this.Controls.Add(this.loginButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -128,6 +177,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button regButton;
         private System.Windows.Forms.TextBox pswBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
