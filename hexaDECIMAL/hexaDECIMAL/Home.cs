ad4d8551b022ed9edcc9d55e8c2037f588ba25a9
haxSnake <silentsnake1986@gmail.com>
2018-09-14 11:36:42 +0100

    userId global added, home changes

--- hexaDECIMAL/hexaDECIMAL/Home.cs
+++ hexaDECIMAL/hexaDECIMAL/Home.cs
@@ -20,6 +20,13 @@ namespace hexaDECIMAL.UserControlPanel
         private Label label1;
         private Label label5;
         private Label label4;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TableLayoutPanel tableLayoutPanel1;
         User user = new User();
 
         public Home()
@@ -37,7 +44,14 @@ namespace hexaDECIMAL.UserControlPanel
 
                 // creating cmd using MySql and conn
                 MySqlCommand cmd = user.dbCon.CreateCommand();
-                cmd.CommandText = "select * from user, account";
+
+                string userIdQuery = "select * from user, account where (user.userId=@userId and account.userId=@userId)";
+                cmd.Parameters.Add(new MySqlParameter("@userId", Form1.userId));
+                cmd.CommandText = userIdQuery;
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
                 mdr = cmd.ExecuteReader(); // Reading string from database into the reader
 
                 // getting data from db and display
@@ -78,6 +92,15 @@ namespace hexaDECIMAL.UserControlPanel
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -132,9 +155,99 @@ namespace hexaDECIMAL.UserControlPanel
             this.label4.TabIndex = 9;
             this.label4.Text = "Name";
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label6, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label7, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1468, 537);
+            this.tableLayoutPanel2.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ravie", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(253, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(962, 155);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Welcome to";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ravie", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(205, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1058, 155);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "haxaDecimal";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Ravie", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(472, 460);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(524, 45);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "The Money Saving App";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(680, 55);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 39);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Name";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(623, 150);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(221, 39);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Your Balance";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 537);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1468, 300);
+            this.tableLayoutPanel1.TabIndex = 16;
+            // 
             // Home
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(154)))), ((int)(((byte)(70)))));
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -142,6 +255,10 @@ namespace hexaDECIMAL.UserControlPanel
             this.Controls.Add(this.label4);
             this.Name = "Home";
             this.Size = new System.Drawing.Size(1468, 837);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
